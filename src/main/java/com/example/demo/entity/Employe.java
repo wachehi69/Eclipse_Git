dded77fbff04972363d01e74e48ba470e7e8f63a
wachehi69 <wachehi@hotmail.com>
2022-09-02 12:26:18 +0200

    initialisation du projet

---
+++ src/main/java/com/example/demo/entity/Employe.java
@@ -0,0 +1,50 @@
+package com.example.demo.entity;
+
+public class Employe {
+	
+	private Long id;
+	private String nom;
+	private String prenom;
+	
+	public Employe() {
+		super();
+	}
+
+	public Employe(Long id, String nom, String prenom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	@Override
+	public String toString() {
+		return "Employe [id=" + id + ", nom=" + nom + ", prenom=" + prenom + "]";
+	}
+
+
+}
