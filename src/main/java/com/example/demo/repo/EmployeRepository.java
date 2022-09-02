47aa51fe32b46e24b788e2b7485b90768b8f5918
wachehi69 <wachehi6905@gmail.com>
2022-09-02 17:22:00 +0200

    premier comit

--- src/main/java/com/example/demo/repo/EmployeRepository.java
+++ src/main/java/com/example/demo/repo/EmployeRepository.java
@@ -9,6 +9,7 @@ import com.example.demo.entity.Employe;
 public interface EmployeRepository extends JpaRepository<Employe, Long>{
 	
 	public Employe findEmployeById(Long id) ;
+	
 	public List<Employe> findEmployeBynom(String nom);
 
 }
