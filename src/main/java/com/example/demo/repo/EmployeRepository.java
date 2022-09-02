3b9a3bb1c0a24c4b2be59ab1561005b2b5cc816c
wachehi69 <wachehi@hotmail.com>
2022-09-02 13:11:55 +0200

    methode findEmploye

--- src/main/java/com/example/demo/repo/EmployeRepository.java
+++ src/main/java/com/example/demo/repo/EmployeRepository.java
@@ -1,11 +1,14 @@
 package com.example.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Employe;
 
 public interface EmployeRepository extends JpaRepository<Employe, Long>{
 	
-	public Employe findEmployeByNom(String nom) ;
+	public Employe findEmployeById(Long id) ;
+	public List<Employe> findEmployeBynom(String nom);
 
 }
